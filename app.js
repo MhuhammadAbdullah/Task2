3882ad960b9427a159df423113eedaa973107e7f
MhuhammadAbdullah <abaloch0033@gmail.com>
2024-09-27 11:30:16 +0500

    updated

---
+++ app.js
@@ -0,0 +1,20 @@
+// task 1
+
+// var str = "hello world";
+// var cap = str.slice(0, 1).toUpperCase() + str.slice(1, 6) + str.slice(6, 7).toUpperCase() + str.slice(7);
+// console.log(cap);
+
+
+
+// task 2
+
+// var str = "saylani mass it training program";
+// for (var i = 0; i < str.length; i++) {
+//     if (str.slice(i, i + 8) === "training") {
+//         console.log("found!");
+//         break;
+//     }
+// } 
+
+
+
