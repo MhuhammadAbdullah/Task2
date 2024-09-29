41da18ae797886e0009fa295cf656088242a8406
MhuhammadAbdullah <abaloch0033@gmail.com>
2024-09-29 12:06:21 +0500

    updated

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 // task 1
 
 // var str = "hello world";
-// var cap = str.slice(0, 1).toUpperCase() + str.slice(1, 6) + str.slice(6, 7).toUpperCase() + str.slice(7);
-// console.log(cap);
+// var firstcap = str.slice(0, 1).toUpperCase() + str.slice(1, 6)
+// var secondcap = str.slice(6, 7).toUpperCase() + str.slice(7);
+// console.log(firstcap + secondcap);
 
 
 
 // task 2
 
-// var str = "saylani mass it training program";
+// var str = "hello, i am saylani mass it training student";
 // for (var i = 0; i < str.length; i++) {
-//     if (str.slice(i, i + 8) === "training") {
-//         console.log("found!");
+//     if (str.slice(i, i + 7) === "saylani") {
+//         console.log("saylani found!");
 //         break;
 //     }
-// } 
 
+// }
+
+
+
+// task 3
+
+// var str = "hello, i am saylani mass it training student";
+// var index = str.indexOf("saylani");
+
+// if (index !== -1) {
+//     console.log("saylani found at index: " + index);
+// }
+
+
+
+//  task 4
+
+// var text = "hello i am saylani mass it training student in malir campous"
+// var word = text.replace("saylani mass it training", "SMIT")
+// console.log(word);
 
 
